608d3eba20dbbdc650eb1cd4b87b63b81ed614e4
Leead Jacobowitz <leead123@gmail.com>
2023-04-12 22:11:44 +0300

    task2/done

--- Ex1_task_shell/shell2.c
+++ Ex1_task_shell/shell2.c
@@ -13,10 +13,11 @@ char *token;
 char *outfile;
 int i, fd, amper, redirect, retid, status, append_redirect;
 char *argv[10];
+char prompt[1024] = "hello";
 
 while (1)
 {
-    printf("hello: ");
+    printf("%s: ", prompt);
     fgets(command, 1024, stdin);
     command[strlen(command) - 1] = '\0';
 
@@ -53,6 +54,12 @@ while (1)
         append_redirect = 1;
         argv[i-2] = NULL;
         outfile = argv[i-1];
+        // fd = open(outfile, O_WRONLY | O_CREAT | O_APPEND, 0660);
+        // close(STDOUT_FILENO);
+        // dup(fd);
+        // close(STDERR_FILENO);
+        // dup(fd);
+        // close(fd);
     }
 
     else if(!strcmp(argv[i-2], "2>")){
@@ -65,7 +72,13 @@ while (1)
         close(fd);
     }
     else 
-        redirect = 0; 
+        redirect = append_redirect = 0; 
+
+    /* Command to change the prompt*/
+    if(i == 3 && !strcmp(argv[0], "prompt") && !strcmp(argv[1], "=")){
+        strncpy(prompt, argv[2], 1024);
+        continue;
+    }
 
     /* for commands not part of the shell command language */ 
 
@@ -87,9 +100,17 @@ while (1)
         }
 
         execvp(argv[0], argv);
+        fprintf(stderr, "%s: command not found\n", argv[0]);
+        exit(127);
     }
     /* parent continues here */
     if (amper == 0)
         retid = wait(&status);
+
+    // Redirect stdout back to the terminal
+        fd = open("/dev/tty", O_WRONLY);
+        close(STDOUT_FILENO);
+        dup(fd);
+        close(fd);
 }
 }
