f076368aa88d8268d3b20756a99abfd080d9efc8
Leead Jacobowitz <leead123@gmail.com>
2023-04-16 16:55:43 +0300

    task9/done (pipe)

--- Ex1_task_shell/shell2.c
+++ Ex1_task_shell/shell2.c
@@ -23,9 +23,10 @@ int main() {
 
     char *command = malloc(1024);
     char last_command[1024] = "";
+    char *cmds[NUM_STRINGS];
     char *token;
     char *outfile;
-    int i, fd, amper, redirect, retid, status, append_redirect;
+    int i, fd, amper, redirect, retid, status, append_redirect, numPipes, num_cmds;
     char *argv[NUM_STRINGS];
     char prompt[1024] = "hello";
 
@@ -64,16 +65,16 @@ int main() {
 
         /* parse command line */
         printf("Command: %s\n", command);
+
+        //////////////////////////////////////////////////////////////
         if(strchr(command, '|') != NULL){
             printf("Pipe Command\n");
             // Command is a pipe command:
-            int num_cmds = 0;
-            char *cmds[NUM_STRINGS];
+            num_cmds = 0;
             for(i=0; i<NUM_STRINGS; i++){
                 cmds[i] = NULL;
-                cmds[i] = malloc(MAX_LENGTH * sizeof(char));
+                cmds[i] = (char *)malloc(MAX_LENGTH * sizeof(char));
             }
-            char *buff[1024];
             token = strtok(command, "|");
             while(token != NULL && num_cmds < NUM_STRINGS){
                 while(*token == ' ') {
@@ -84,91 +85,38 @@ int main() {
                 token = strtok(NULL, "|");
                 num_cmds++;
             }
-            int numPipes = num_cmds -1;
+            numPipes = num_cmds -1;
             printf("numPipes: %d\n", numPipes);
             printf("num_cmds: %d\n", num_cmds);
-
-            int in=0;
-            int pipe_fd[2];
-            pid_t pid;
-
-            // Create pipes and fork processes
-            for(i=0; i<num_cmds; i++){
-                //create a new pipe:
-                pipe(pipe_fd);
-
-                //fork a new proccess:
-                pid = fork();
-                if(pid < 0){
-                    perror("fork error");
-                    continue;
-                }
-                else if(pid == 0){
-                    // entering child proccess:
-                    char* cmd = cmds[i];
-
-                    //closing the read end of the prev pipe:
-                    close(in);
-
-                    // If this is not the last command, redirect the write end of this pipe to the input of the next command:
-                    if(i < num_cmds-1){
-                        dup2(pipe_fd[1], STDOUT_FILENO);
-                    }
-
-                    // If this is not the first command, redirect the read end of the previous pipe to the input of this command
-                    if (i > 0) {
-                        dup2(pipe_fd[0], STDIN_FILENO);
-                    }
-
-                    // Close both ends of this pipe
-                    close(pipe_fd[0]);
-                    close(pipe_fd[1]);
-
-                    // Execute the command
-                    char *args[10];
-                    i = 0;
-                    token = strtok(cmd, " ");
-                    while (token != NULL)
-                    {
-                        args[i] = token;
-                        token = strtok(NULL, " ");
-                        i++;
-                    }
-                    args[i] = NULL;
-                    if (execvp(args[0], args) == -1) {
-                    perror("Error executing command");
-                    exit(EXIT_FAILURE);
-                }
-
-                    exit(EXIT_SUCCESS);;
-                }
-                else{
-                    // entering parent proccess:
-                    // Close the write end of the previous pipe
-                    close(pipe_fd[1]);
-
-                    // Redirect standard error to standard output
-                    dup2(STDOUT_FILENO, STDERR_FILENO);
-
-                    // If this is not the first command, save the read end of this pipe as the input for the next command
-                    if (i > 0) {
-                        in = pipe_fd[0];
-                    }
-                }
-            }
-            // Wait for all child processes to finish
-            while (wait(NULL) > 0);
-
-            for(i=0; i<NUM_STRINGS; i++){
-                free(cmds[i]);
-            }
-            
-        }
+        }///////////////////////////////////////////////////////////////
 
         else{
+            // if command is a normal command:
+            cmds[0] = (char *)malloc(MAX_LENGTH * sizeof(char));
+            strcpy(cmds[0], command);
+            num_cmds = 1;
+            numPipes = 0;
+        }
+
+        int in=0;
+        int pipe_fd[2*numPipes];
+        pid_t pid;
+
+        for(i = 0; i < (numPipes); i++){
+            if(pipe(pipe_fd + i*2) < 0) {
+                perror("couldn't pipe");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        int pipe_index=0;
+        // Create pipes and fork processes
+        for(int k=0; k<num_cmds; k++){
+            char* cmd = cmds[k];
+
             // command is a normal command
             i = 0;
-            token = strtok (command," ");
+            token = strtok (cmd," ");
             for(i=0; i<NUM_STRINGS; i++){
                 // argv[i] = NULL;
                 argv[i] = malloc(MAX_LENGTH * sizeof(char));
@@ -187,182 +135,175 @@ int main() {
                 
                 token = strtok (NULL, " ");
             }
+
             argv[i] = NULL;
             redirect = 0;
             append_redirect = 0;
             outfile = NULL;
-        }
+        
         
 
-        /* Is command empty */
-        if (argv[0] == NULL){
-            // printf("\n");
-            continue;
-        }
-            
-
-        /* Does command line end with & */ 
-        if (! strcmp(argv[i - 1], "&")) {
-            amper = 1;
-            argv[i - 1] = NULL;
-            // printf("\n");
-        } else {
-            amper = 0; 
-        }
-
-        /* Does command line contain ">" or ">>" */ 
-        if(i > 1){
-            if (! strcmp(argv[i - 2], ">") || (! strcmp(argv[i - 2], ">>"))) {
-                redirect = 1;
-                if((! strcmp(argv[i - 2], ">>"))){
-                    append_redirect =1 ;
-                }
-                outfile = argv[i - 1];
-                argv[i - 2] = NULL;
-                argv[i-1] = NULL;
+            /* Is command empty */
+            if (argv[0] == NULL){
+                // printf("\n");
+                continue;
             }
-            /* Does command line contain "2>" */ 
-            else if(!strcmp(argv[i-2], "2>")){
-                redirect = 3;
-                argv[i-2] = NULL;
-                outfile = argv[i-1];
-                fd = creat(outfile, 0660);
-                close(STDERR_FILENO);
-                dup(fd);
-                close(fd);
+
+            /* Does command line end with & */ 
+            if (! strcmp(argv[i - 1], "&")) {
+                amper = 1;
+                argv[i - 1] = NULL;
                 // printf("\n");
             } else {
-                redirect = append_redirect = 0; 
+                amper = 0; 
             }
-        }
-        
 
-        
-
-        /* Command to change the prompt*/
-        if(i == 3 && !strcmp(argv[0], "prompt") && !strcmp(argv[1], "=")){
-            strncpy(prompt, argv[2], 1024);
-            continue;
-        }
-
-        /* Command to echo status */
-        if (!strcmp(argv[0], "echo") && !strcmp(argv[1], "$?")) {
-            printf("%d\n", WEXITSTATUS(status));
-            continue;
-        }
-
-        /* Command to echo arguments*/
-        if(!strcmp(argv[0], "echo")){
-            for(int j=1; j<i; j++){
-                if(argv[j][0] == '$'){
-                    char *var = argv[j] + 1;
-                    char *str = getenv(var);
-                    printf("%s ", str);
+            /* Does command line contain ">" or ">>" */ 
+            if(i > 1){
+                if (! strcmp(argv[i - 2], ">") || (! strcmp(argv[i - 2], ">>"))) {
+                    redirect = 1;
+                    if((! strcmp(argv[i - 2], ">>"))){
+                        append_redirect =1 ;
+                    }
+                    outfile = argv[i - 1];
+                    argv[i - 2] = NULL;
+                    argv[i-1] = NULL;
                 }
-                else
-                    printf("%s ", argv[j]);
+                /* Does command line contain "2>" */ 
+                else if(!strcmp(argv[i-2], "2>")){
+                    redirect = 3;
+                    argv[i-2] = NULL;
+                    outfile = argv[i-1];
+                    fd = creat(outfile, 0660);
+                    close(STDERR_FILENO);
+                    dup(fd);
+                    close(fd);
+                    // printf("\n");
+                } else {
+                    redirect = append_redirect = 0; 
+                }
             }
-            printf("\n");
-            continue;
-        }
 
-        /* Command to change directory */
-        if (!strcmp(argv[0], "cd")) {
-            if (i < 2) {
-                fprintf(stderr, "cd: missing operand\n");
+            /* Command to change the prompt*/
+            if(i == 3 && !strcmp(argv[0], "prompt") && !strcmp(argv[1], "=")){
+                strncpy(prompt, argv[2], 1024);
+                continue;
             }
-            else if (chdir(argv[1]) != 0) {
-                fprintf(stderr, "cd: %s: No such file or directory\n", argv[1]);
+
+            /* Command to echo status */
+            if (!strcmp(argv[0], "echo") && !strcmp(argv[1], "$?")) {
+                printf("%d\n", WEXITSTATUS(status));
+                continue;
             }
-            continue;
-        }
 
-    
-        if(i == 3 && argv[0][0] == '$' && !strcmp(argv[1], "=")){
-            char *var = argv[0] + 1;
-            setenv(var, argv[2], 1);
-            continue;
-        }
+            /* Command to echo arguments*/
+            if(!strcmp(argv[0], "echo")){
+                for(int j=1; j<i; j++){
+                    if(argv[j][0] == '$'){
+                        char *var = argv[j] + 1;
+                        char *str = getenv(var);
+                        printf("%s ", str);
+                    }
+                    else
+                        printf("%s ", argv[j]);
+                }
+                printf("\n");
+                continue;
+            }
 
-        if(!strcmp(argv[0], "read")){
-            char* str = getenv(argv[1]);
-            printf("%s \n", str);
-            continue;
-        }
+            /* Command to change directory */
+            if (!strcmp(argv[0], "cd")) {
+                if (i < 2) {
+                    fprintf(stderr, "cd: missing operand\n");
+                }
+                else if (chdir(argv[1]) != 0) {
+                    fprintf(stderr, "cd: %s: No such file or directory\n", argv[1]);
+                }
+                continue;
+            }
 
+        
+            if(i == 3 && argv[0][0] == '$' && !strcmp(argv[1], "=")){
+                char *var = argv[0] + 1;
+                setenv(var, argv[2], 1);
+                continue;
+            }
 
+            if(!strcmp(argv[0], "read")){
+                char* str = getenv(argv[1]);
+                printf("%s \n", str);
+                continue;
+            }
 
+            /* for commands not part of the shell command language */ 
+            pid_t pid = fork();
+            if (pid == 0) {
+                //if not last command
+                if(k + 1 != num_cmds){
+                    if(dup2(pipe_fd[pipe_index + 1], 1) < 0){
+                        perror("dup2");
+                        exit(EXIT_FAILURE);
+                    }
+                }
 
+                //if not first command&& pipe_index!= 2*numPipes
+                if(pipe_index != 0 ){
+                    if(dup2(pipe_fd[pipe_index-2], 0) < 0){
+                        perror(" dup2");
+                        exit(EXIT_FAILURE);
 
+                    }
+                }
+                // Entering child proccess: 
+                /* redirection of IO ? */
+                if(redirect){
+                    if(append_redirect){
+                        fd = open(outfile, O_WRONLY | O_CREAT | O_APPEND | O_RDONLY, 0644);
+                    } else{
+                        fd = open(outfile, O_CREAT | O_TRUNC | O_WRONLY | O_RDONLY, 0644);
+                    }
+                    
+                    // close(STDOUT_FILENO);
+                    dup2(fd, STDOUT_FILENO);
+                    close(fd);
+                }
 
+                for(int z = 0; z < 2*numPipes; z++){
+                    close(pipe_fd[z]);
+                }
 
-        /* for commands not part of the shell command language */ 
-        pid_t pid = fork();
-        if (pid == 0) {
-            // Entering child proccess: 
-            /* redirection of IO ? */
-            if(redirect){
-                if(append_redirect){
-                    fd = open(outfile, O_WRONLY | O_CREAT | O_APPEND | O_RDONLY, 0644);
-                } else{
-                    fd = open(outfile, O_CREAT | O_TRUNC | O_WRONLY | O_RDONLY, 0644);
+                if (execvp(argv[0], argv) == -1) {
+                    perror("Error executing command");
+                    exit(EXIT_FAILURE);
                 }
                 
-                // close(STDOUT_FILENO);
-                dup2(fd, STDOUT_FILENO);
-                close(fd);
+                
             }
-            if (execvp(argv[0], argv) == -1) {
-                perror("Error executing command");
-                exit(EXIT_FAILURE);
-            }
-            } else if (pid < 0) {
+
+            else if (pid < 0) {
                 // Error
                 perror("fork");
-            } else {
-                // In parent process
-                if (! amper) {
-                    // Wait for child process to finish
-                    waitpid(pid, &status, 0);
-                }
-            }
+            } 
+                
+            // else {
+            //     // In parent process
+            //     if (! amper) {
+            //         // Wait for child process to finish
+            //         waitpid(pid, &status, 0);
+            //     }
+            // }
+
+            pipe_index+=2;
         }
-        free(command);
+
+        for(i = 0; i < 2 * numPipes; i++){
+            close(pipe_fd[i]);
+        }
+
+        for(i = 0; i < numPipes + 1; i++){
+            wait(&status);
+        }
+         
     }
-        //     else{
-        //         //redirect = 1 or 3
-        //         fd == creat(outfile, 0660);
-        //         close(STDOUT_FILENO);
-        //         dup(fd);
-        //         close(fd);
-        //     }
-
-        //     int exitstatus = execvp(argv[0], argv);
-        //     fprintf(stderr, "%s: command not found\n", argv[0]);
-        //     exit(exitstatus);
-        // }
-        // else{
-        //     // Entering main proccess:
-        //     /* parent continues here */
-        // if (amper == 0)
-        //     retid = wait(&status);
-
-        // // Redirect stdout back to the terminal
-        //     fd = open("/dev/tty", O_WRONLY);
-        //     close(STDOUT_FILENO);
-        //     dup(fd);
-        //     close(fd);
-        // }
-
-
-        // /* parent continues here */
-        // if (amper == 0)
-        //     retid = wait(&status);
-
-        // // Redirect stdout back to the terminal
-        //     fd = open("/dev/tty", O_WRONLY);
-        //     close(STDOUT_FILENO);
-        //     dup(fd);
-        //     close(fd);
-    // }
-// }
+    free(command); 
+}
